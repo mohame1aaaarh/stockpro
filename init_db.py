a35361b4c7f46f13e754c22786635fd90a93b05b
mohame1aaaarh <mohamed1alaalmy@gmail.com>
2026-03-01 15:26:42 +0200

    end task 2 initialize database schema

--- init_db.py
+++ init_db.py
@@ -1,1 +1,16 @@
 import sqlite3
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+
+def init_db():
+    conn = sqlite3.connect("database/stockpro.db")
+    
+    with open("database/database.sql", "r", encoding="utf-8") as f:
+        sql = f.read()
+    
+    conn.executescript(sql)
+    conn.commit()
+    conn.close()
+    print("✅ Database created successfully!")
+
+init_db()
